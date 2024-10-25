bab7b1bc178fc41b720573222f260eef0e19d5b0
Mesude Gökpınar <gokpinarmesude@gmail.com>
2024-10-25 22:50:48 +0300

    update header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,26 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../UserContext';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
     const { state } = useContext(UserContext);
     const { user } = state;
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        if (user) {
+            navigate('/main'); // Ana sayfaya yönlendir
+        } else {
+            alert("Lütfen önce giriş yapın veya kayıt olun."); // Kullanıcıyı bilgilendirme
+        }
+    };
 
     return (
         <header style={headerStyle}>
-            <h1 style={titleStyle}>
-                <Link to="/" style={linkStyle}>BookWave</Link>
-                {user && <Link to="/user" style={linkStyle}>User Page</Link>} {/* User Page linki sadece giriş yapıldıysa gözükecek */}
+            <h1 style={titleStyle} onClick={handleLogoClick}>
+                <span style={linkStyle}>BookWave</span>
             </h1>
+            {user && <Link to="/user" style={linkStyle}>User Page</Link>} {/* User Page linki sadece giriş yapıldıysa gözükecek */}
         </header>
     );
 };
@@ -25,9 +34,12 @@ const headerStyle = {
     backgroundColor: '#282c34',
     color: 'white',
 };
+
 const titleStyle = {
     margin: 0,
+    cursor: 'pointer', // Başlık metni tıklanabilir olacak
 };
+
 const linkStyle = {
     color: 'white',
     textDecoration: 'none',
